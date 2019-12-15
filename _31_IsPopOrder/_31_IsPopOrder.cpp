9f330ba4775016b565cf09b3afe4747470726b30
WuZeming <1215772385@qq.com>
2019-12-15 16:03:44 +0800

    test 31

---
+++ _31_IsPopOrder/_31_IsPopOrder.cpp
@@ -0,0 +1,117 @@
+// _31_IsPopOrder.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <cstdio>
+#include <stack>
+
+bool IsPopOrder(const int* pPush, const int* pPop, int nLength)
+{
+	if (pPush == nullptr || pPop == nullptr || nLength <=0)
+	{
+		return false;
+	}
+	std::stack<int> stackData;
+	for (int i = 0,j = 0; i < nLength; i++)
+	{
+		stackData.push(pPush[i]);
+		while(!stackData.empty() && stackData.top() == pPop[j])
+		{
+			stackData.pop();
+			++j;
+		}
+	}
+	if (stackData.empty())
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+// ====================测试代码====================
+void Test(const char* testName, const int* pPush, const int* pPop, int nLength, bool expected)
+{
+	if (testName != nullptr)
+		printf("%s begins: ", testName);
+
+	if (IsPopOrder(pPush, pPop, nLength) == expected)
+		printf("Passed.\n");
+	else
+		printf("failed.\n");
+}
+
+void Test1()
+{
+	const int nLength = 5;
+	int push[nLength] = { 1, 2, 3, 4, 5 };
+	int pop[nLength] = { 4, 5, 3, 2, 1 };
+
+	Test("Test1", push, pop, nLength, true);
+}
+
+void Test2()
+{
+	const int nLength = 5;
+	int push[nLength] = { 1, 2, 3, 4, 5 };
+	int pop[nLength] = { 3, 5, 4, 2, 1 };
+
+	Test("Test2", push, pop, nLength, true);
+}
+
+void Test3()
+{
+	const int nLength = 5;
+	int push[nLength] = { 1, 2, 3, 4, 5 };
+	int pop[nLength] = { 4, 3, 5, 1, 2 };
+
+	Test("Test3", push, pop, nLength, false);
+}
+
+void Test4()
+{
+	const int nLength = 5;
+	int push[nLength] = { 1, 2, 3, 4, 5 };
+	int pop[nLength] = { 3, 5, 4, 1, 2 };
+
+	Test("Test4", push, pop, nLength, false);
+}
+
+// push和pop序列只有一个数字
+void Test5()
+{
+	const int nLength = 1;
+	int push[nLength] = { 1 };
+	int pop[nLength] = { 2 };
+
+	Test("Test5", push, pop, nLength, false);
+}
+
+void Test6()
+{
+	const int nLength = 1;
+	int push[nLength] = { 1 };
+	int pop[nLength] = { 1 };
+
+	Test("Test6", push, pop, nLength, true);
+}
+
+void Test7()
+{
+	Test("Test7", nullptr, nullptr, 0, false);
+}
+
+int main(int argc, char* argv[])
+{
+	Test1();
+	Test2();
+	Test3();
+	Test4();
+	Test5();
+	Test6();
+	Test7();
+
+	return 0;
+}
