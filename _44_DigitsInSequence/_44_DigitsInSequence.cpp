4df6c68c5c98d75956649ac8c0facd157400c7dc
Ming <wuzm_sjtu@qq.com>
2020-02-12 22:35:35 +0800

    test44

---
+++ _44_DigitsInSequence/_44_DigitsInSequence.cpp
@@ -0,0 +1,91 @@
+/*******************************************************************
+Copyright(c) 2016, Harry He
+All rights reserved.
+Distributed under the BSD license.
+(See accompanying file LICENSE.txt at
+https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
+*******************************************************************/
+
+//==================================================================
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+// 作者：何海涛
+//==================================================================
+
+// 面试题44：数字序列中某一位的数字
+// 题目：数字以0123456789101112131415…的格式序列化到一个字符序列中。在这
+// 个序列中，第5位（从0开始计数）是5，第13位是1，第19位是4，等等。请写一
+// 个函数求任意位对应的数字。
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int countOfIntegers(int digit)
+{
+	if (digit == 1)
+		return 10;
+	return 9 * pow(10, digit - 1);
+}
+
+int beginNumber(int digit)
+{
+	if (digit == 1)
+	{
+		return 0;
+	}
+	return pow(10, digit - 1);
+}
+
+int digitAttheIndex(int index, int digit)
+{
+	int begin = beginNumber(digit);
+	int theNumber = begin + index / digit;
+	int theIndexFromRight =digit- index%digit;
+	for (int i = 1; i < theIndexFromRight; i++)
+		theNumber /= 10;
+	return theNumber % 10;
+
+}
+
+int digitAtIndex(int index)
+{
+	if (index < 0)
+		return -1;
+	int digit = 1;
+	while (true)
+	{
+		int numbers = countOfIntegers(digit);
+
+		if (numbers*digit > index)
+			return digitAttheIndex(index, digit);
+		index = index - digit*numbers;
+		digit++;
+	}
+	return -1;
+}
+
+
+// ====================测试代码====================
+void test(const char* testName, int inputIndex, int expectedOutput)
+{
+	if (digitAtIndex(inputIndex) == expectedOutput)
+		cout << testName << " passed." << endl;
+	else
+		cout << testName << " FAILED." << endl;
+}
+
+
+int main()
+{
+	test("Test1", 0, 0);
+	test("Test2", 1, 1);
+	test("Test3", 9, 9);
+	test("Test4", 10, 1);
+	test("Test5", 189, 9);  // 数字99的最后一位，9
+	test("Test6", 190, 1);  // 数字100的第一位，1
+	test("Test7", 1000, 3); // 数字370的第一位，3
+	test("Test8", 1001, 7); // 数字370的第二位，7
+	test("Test9", 1002, 0); // 数字370的第三位，0
+	return 0;
+}
